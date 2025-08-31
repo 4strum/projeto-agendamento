77290f89180a341dcfe48db1c6a3e6530534729a
João Pedro <leon.jpbs@gmail.com>
2025-08-31 19:31:06 -0300

    Criada a migration do agendamento

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -9,5 +9,9 @@ class Cliente extends Model
     protected $table = 'clientes';
     protected $fillable = ['nome', 'email', 'telefone', 'cpf'];
 
+    public function agendamentos()
+    {
+        return $this->hasMany(Agendamento::class);
+    }
 
 }
